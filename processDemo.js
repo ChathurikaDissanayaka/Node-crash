7545965e820d7e95664f5fb8eea793979b08b124
Sachini <sachichathudissanayaka@gmail.com>
2024-10-28 21:08:56 +0530

    Add changes

---
+++ processDemo.js
@@ -0,0 +1,29 @@
+// console.log(process.argv)
+// console.log(process.argv[3])
+
+// process.env
+console.log(process.env.USERNAME);
+
+// pid
+console.log(process.pid);
+
+// cwd
+console.log(process.cwd());
+
+// title
+console.log(process.title);
+
+// memoryUsage()
+console.log(process.memoryUsage());
+
+// uptime()
+console.log(process.uptime());
+
+process.on("exit", (code) => {
+  console.log(`about to exit with code: ${code}`);
+});
+
+// exit()
+console.log(process.exit(0));
+
+console.log("Hello");
